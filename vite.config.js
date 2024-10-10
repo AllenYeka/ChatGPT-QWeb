650a15224a82f79b11d04a0c83053405d921942c
wrq <xdhvuexgd@icloud.com>
2024-10-10 14:10:47 +0800

    直接调用api.openai-hk.com

--- vite.config.js
+++ vite.config.js
@@ -17,7 +17,7 @@ export default defineConfig({
   server: {
     port: 5380,
     proxy: {
-      "/v1/chat/completions": { target: 'https://wrq6827.top:443/', secure: false, changeOrigin: true },
+      "/api/v1/chat/completions": { target: 'https://api.openai-hk.com/', secure: false, changeOrigin: true, rewrite: (path) => path.replace('/api', '') },
     },
   },
 })
